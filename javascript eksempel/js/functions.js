b3150afadfa17d0fc0672ccfc596d88a8f456523
Stine <stine.jonassen.foyen@vlfk.no>
2023-10-19 11:28:56 +0200

    Create functions.js

---
+++ javascript eksempel/js/functions.js
@@ -0,0 +1,7 @@
+function buttonClick() {
+    document.getElementById("header").style.display = "none";
+}
+
+function showImage() {
+    document.getElementById("icecream").style.display = "block";
+}
