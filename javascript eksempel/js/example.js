953e7ce2d732f32be228eccc79627a2383bb1ecb
Stine <stine.jonassen.foyen@vlfk.no>
2023-10-19 08:27:01 +0200

    added files

---
+++ javascript eksempel/js/example.js
@@ -0,0 +1,18 @@
+let header = document.getElementById("header");
+header.innerText = "Hei";
+header.style.color = "green";
+
+let number1 = 30; 
+let number2 = 20; 
+let result; 
+let text; 
+
+result = number1 + number2; 
+text = "Resultatet blir: " + result;
+
+console.log(text);
+
+alert("Hallo!");
+
+let firstName = prompt("Kva er navnet ditt?"); 
+header.innerText = "Velkommen, " + firstName; 
