56fc11312c533a124632b4f0b685acb2905277f3
Stine <stine.jonassen.foyen@vlfk.no>
2023-11-15 09:50:32 +0100

    added new example

---
+++ javascript eksempel/js/151123.js
@@ -0,0 +1,9 @@
+let age = prompt("Skriv inn alderen din: "); 
+
+if (age >= 18) {
+    alert("Du kan ta førarkort for bil");
+}
+else if (age >= 16) {
+    alert("Du kan ta førarkort på moped"); 
+}
+
--- javascript eksempel/js/example.js
+++ javascript eksempel/js/example.js
@@ -4,7 +4,8 @@ header.style.color = "green";
 
 const temp = 23; 
 
-let number1 = 30; 
+let number1 = 30;
+
 let number2 = 20; 
 let result; 
 
@@ -20,6 +21,8 @@ result = number1 + 23;
 result = "hei"; 
 
 result = 100; 
+
+// Einlinje kommentar 
 
 /*
 text = "Resultatet blir: " + result;
